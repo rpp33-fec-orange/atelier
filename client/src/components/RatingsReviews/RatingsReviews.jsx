9e36e2a93188dc50450996b574a2b7c29bd96834
Tan Ha <tandha101@gmail.com>
2022-02-11 02:24:09 -0800

    structuring component with divs

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 const RatingsReviews = (props) => (
   <div>
     <h4 id='ratings-reviews'>Ratings and Reviews</h4>
-    <div>
-    <div> Review List
-      <div> Review #1 </div>
-      <div> Review #2 </div>
-      <div> More Reviews</div>
-    </div>
-
+      <h3>Review List</h3>
+        <div> Review #1 </div>
+        <div> Review #2 </div>
+        <div> More Reviews</div>
+        <div>Add Reviews</div>
+      <h3>Star Reviews</h3>
+        <div>Star Number</div>
+        <div>Stars</div>
+        <div>Percent Recommended</div>
+      <h3>Sliders</h3>>
+        <div>Size slider</div>
+        <div>Comfort Slider</div>
   </div>
-  </div>
-
 )
 
 export default RatingsReviews;
