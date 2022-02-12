531f100c8bc77de40ee868a62f4b9cc9aeb434fd
Bhrigu <joshi.bhrigu25@gmail.com>
2022-02-12 09:21:59 -0500

    'Initial widget for Related Products Complete'

---
+++ client/src/components/RelatedProducts/stockImages.jsx
@@ -0,0 +1,16 @@
+var relatedProductImages = [
+  "https://media.istockphoto.com/photos/pink-polo-tshirt-isolated-on-white-background-picture-id1129141870?k=20&m=1129141870&s=612x612&w=0&h=lIbzWZIBFfo7tuQVcM7BkQkbzZ9ARdUCo3Tp5SZPjR8=",
+  "https://media.istockphoto.com/photos/front-of-a-clean-polo-tshirt-picture-id501366958?k=20&m=501366958&s=612x612&w=0&h=iMTTgQw0apH7oe5k1mpL5fnM1F5H8pRs0ehSPMmbwqE=",
+  "https://media.istockphoto.com/photos/womens-pink-polo-shirt-on-white-picture-id610041812?k=20&m=610041812&s=612x612&w=0&h=yAJuq7cvLoVfGdJXUzarXClWMCaVWLyBAh0atoXq188=",
+  "https://media.istockphoto.com/photos/blank-white-tshirt-front-with-clipping-path-picture-id482948743?k=20&m=482948743&s=612x612&w=0&h=11DSbRFCtpwGzged_UxizZp_mdgFaPxxydeWrcDj7No="
+];
+
+var yourOutfitImages = [
+  "https://media.istockphoto.com/photos/red-long-sleeved-blank-tshirt-frontisolated-on-white-wclipping-path-picture-id157439629?k=20&m=157439629&s=612x612&w=0&h=vf6Kx7lm_X-MnpCYMCAXD4otcBPRzKkS0ieD4jku3-I=",
+  "https://media.istockphoto.com/photos/front-of-an-unfolded-blank-gray-tshirt-on-white-background-picture-id471184775?k=20&m=471184775&s=612x612&w=0&h=dBlX-PRA3SYuqtJ2ySnR9aXorWUKNzdPSl8ejHHl36U=",
+  "https://media.istockphoto.com/photos/blank-black-tshirt-front-with-clipping-path-picture-id483425141?k=20&m=483425141&s=612x612&w=0&h=L3nzMZItJ2SWuDFZBgrMoC7s4Ze8ETi-cjTI3-jL_Ls=",
+  "https://media.istockphoto.com/photos/green-tshirt-picture-id182874114?k=20&m=182874114&s=612x612&w=0&h=8hO8YSBnIO2kw1WrJoJ9uWQfC4j39au6-Nhf8_uJmw4="
+];
+
+export default {relatedProductImages, yourOutfitImages};
+
