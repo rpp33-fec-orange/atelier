01fd3be06d6c2f46b7a1e7fb714aa979970f909e
Bhrigu <joshi.bhrigu25@gmail.com>
2022-02-12 09:17:02 -0500

    'Initial skeleton of Related Products Widget'

--- tests/client/index.test.js
+++ tests/client/index.test.js
@@ -4,17 +4,17 @@ import App from '../../client/src/App.jsx';
 import ProductOverview from '../../client/src/components/ProductOverview/ProductOverview.jsx';
 import QuestionsAnswers from '../../client/src/components/QuestionsAnswers/QuestionsAnswers.jsx';
 import RatingsReviews from '../../client/src/components/RatingsReviews/RatingsReviews.jsx';
-import RelatedItems from '../../client/src/components/RelatedItems/RelatedItems.jsx';
+import RelatedProducts from '../../client/src/components/RelatedProducts/RelatedProducts.jsx';
 
 describe.only('Main component test', () => {
   const testRenderer = TestRenderer.create(<App />);
   const testInstance = testRenderer.root;
 
-  test('it should contain a class subcomponent called RelatedItems', () => {
+  test('it should contain a class subcomponent called RelatedProducts', () => {
     expect(testInstance.findByType(ProductOverview)).toBeDefined();
     expect(testInstance.findByType(QuestionsAnswers)).toBeDefined();
     expect(testInstance.findByType(RatingsReviews)).toBeDefined();
-    expect(testInstance.findByType(RelatedItems)).toBeDefined();
+    expect(testInstance.findByType(RelatedProducts)).toBeDefined();
   });
 
   it('it should teach us math', function () {
