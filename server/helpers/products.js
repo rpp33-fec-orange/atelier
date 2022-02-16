f00cf259f30fd700b2950f523c747bb402857991
Tan Ha <tandha101@gmail.com>
2022-02-15 21:10:10 -0800

    move config.js and config.example.js from client to root dir

--- server/helpers/products.js
+++ server/helpers/products.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const config = require('../../client/dist/config.js');
+const config = require('../../config.js');
 
 const getProducts = function () {
   return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products', { headers: { 'Authorization': config.API_KEY } })
