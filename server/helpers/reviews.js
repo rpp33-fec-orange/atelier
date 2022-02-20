d5489c554d1143c952740818baccdb1624eaa019
Tan Ha <tandha101@gmail.com>
2022-02-19 16:30:29 -0800

    temporary code

--- server/helpers/reviews.js
+++ server/helpers/reviews.js
@@ -40,17 +40,17 @@ let getReviewsByID = (id) => {
 module.exports.getReviewsByID = getReviewsByID;
 
 
-// app.get('/reviews/', function (req, res) {
-//   getReviewsByID(req.query.product_id)
-//     .then((success) => {
-//       console.log('getting reviews success! data is: ', success.data.results);
-//       // console.log('reviews are: ', data);
-//       res.status(200).send(success.data);
-//     })
-//     .catch((error) => {
-//       console.log('error getting reviews!');
-//     })
-// });
+app.get('/reviews/', function (req, res) {
+  getReviewsByID(req.query.product_id)
+    .then((success) => {
+      console.log('getting reviews success! data is: ', success.data.results);
+      // console.log('reviews are: ', data);
+      res.status(200).send(success.data);
+    })
+    .catch((error) => {
+      console.log('error getting reviews!');
+    })
+});
 
 
 //const getReviewsByID = require('./helpers/reviews.js').getReviewsByID;
