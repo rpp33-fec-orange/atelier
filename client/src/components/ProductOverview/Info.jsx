d890e8805617023432e4eee4d57b2d0057c84e24
Dennis Wang <denniswang.self@gmail.com>
2022-03-01 03:15:48 -0800

    refactored for main/sub photos to show in rectangle aspect ratio

--- client/src/components/ProductOverview/Info.jsx
+++ client/src/components/ProductOverview/Info.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Info = (props) => (
   <div class="info-container" id="Info">
-    <div class="info-item info-item-1">
-      <div id="slogan">{props.productById.slogan}.</div>
-      <div id="description">{props.productById.description}</div>
+    <div class="info-item info-item-1-container">
+      <div class="info-item-1-1" id="slogan">{props.productById.slogan}.</div>
+      <div class="info-item-1-2" id="description">{props.productById.description}</div>
     </div>
     <div class="info-item info-item-2" id="features">
       {props.productById.features.map((singleData) =>
