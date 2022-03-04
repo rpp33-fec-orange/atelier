23be9d77a042e208932d7d644c3e0150a04dd4b9
Dennis Wang <denniswang.self@gmail.com>
2022-03-03 21:30:46 -0800

    finished slider arrow logic and some css

--- client/src/components/ProductOverview/Photos.jsx
+++ client/src/components/ProductOverview/Photos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import zoomIcon from './zoomIcon.png';
 
 class Photos extends React.Component {
   constructor(props) {
@@ -8,9 +9,9 @@ class Photos extends React.Component {
       mainPhotoArray: props.currentStyle.photos,
       mainPhotoIndex: 0,
       mainPhotoURL: props.currentStyle.photos[0].url,
-      subPhotos: props.currentStyle.photos,
+      subPhotosArray: props.currentStyle.photos,
       expanded: false,
-      arrayLeftEnd: false,
+      arrayLeftEnd: true,
       arrayRightEnd: false
     }
     this.photoClick = this.photoClick.bind(this);
@@ -27,14 +28,17 @@ class Photos extends React.Component {
 
   leftClick() {
     let newIndex = this.state.mainPhotoIndex - 1;
-    if (newIndex > -1) {
+    if (this.state.mainPhotoIndex === 0 || newIndex === 0) {
       this.setState({
-        arrayLeftEnd: false,
+        arrayLeftEnd: true,
+        arrayRightEnd: false,
         mainPhotoIndex: newIndex
       });
     } else {
       this.setState({
-        arrayLeftEnd: true,
+        arrayLeftEnd: false,
+        arrayRightEnd: false,
+        mainPhotoIndex: newIndex
       });
     }
     console.log('left, index', this.state.mainPhotoIndex)
@@ -42,14 +46,17 @@ class Photos extends React.Component {
 
   rightClick() {
     let newIndex = this.state.mainPhotoIndex + 1;
-    if (newIndex < this.state.mainPhotoArray.length) {
+    if (this.state.mainPhotoIndex === this.state.mainPhotoArray.length - 1 || newIndex === this.state.mainPhotoArray.length - 1) {
       this.setState({
-        arrayRightEnd: false,
+        arrayRightEnd: true,
+        arrayLeftEnd: false,
         mainPhotoIndex: newIndex
       });
     } else {
       this.setState({
-        arrayRightEnd: true
+        arrayRightEnd: false,
+        arrayLeftEnd: false,
+        mainPhotoIndex: newIndex
       });
     }
     console.log('right, index', this.state.mainPhotoIndex);
@@ -72,7 +79,7 @@ class Photos extends React.Component {
       this.setState({
         currentStyle: this.props.currentStyle,
         mainPhotoURL: this.props.currentStyle.photos[0].url,
-        subPhotos: this.props.currentStyle.photos,
+        subPhotosArray: this.props.currentStyle.photos,
         mainPhotoArray: this.props.currentStyle.photos
       })
     }
@@ -82,21 +89,24 @@ class Photos extends React.Component {
     let mainPhotoArray = this.state.mainPhotoArray;
     let mainPhotoIndex = this.state.mainPhotoIndex;
     let mainPhotoURL = this.state.mainPhotoURL;
-    let subPhotos = this.state.subPhotos;
-    console.log('main photo index: ', mainPhotoIndex, 'left end: ', this.state.arrayLeftEnd, 'right end: ', this.state.arrayRightEnd);
-    if (!this.state.expanded) {
+    let subPhotosArray = this.state.subPhotosArray;
+    let arrayLeftEnd = this.state.arrayLeftEnd;
+    let arrayRightEnd = this.state.arrayRightEnd;
+    let expanded = this.state.expanded;
+    if (!expanded) {
       return (
         <div>
           <div class="styles-item-1-2">
-            {subPhotos.map((photo) =>
-              <img class="styles-item-1-2-1" id="subPhoto" src={photo.url} width="50" height="70" onClick={this.photoClick}></img>
+            {subPhotosArray.map((photo) =>
+              <img class="styles-item-1-2-1" src={photo.url} width="50" height="70" onClick={this.photoClick}></img>
             )}
             <div class="styles-item-1-1 collapsed-magnifying-glass" onClick={this.expandClick}>🔍</div>
+            {/* <img class="styles-item-1-1 collapsed-magnifying-glass" src={zoomIcon} onclick={this.expandClick}></img> */}
           </div>
           <div class="styles-item-1-1">
-            <img class="styles-item-1-1" id="mainPhoto" src={mainPhotoArray[mainPhotoIndex].url} width="360" height="480"></img>
-            <div id="collapsed-left-arrow" onClick={this.leftClick}>←</div>
-            <div id="collapsed-right-arrow" onClick={this.rightClick}>→</div>
+            <img class="styles-item-1-1-1" id="mainPhoto" src={mainPhotoArray[mainPhotoIndex].url} width="360" height="480"></img>
+            {arrayLeftEnd ? <div></div> : <div id="collapsed-left-arrow" onClick={this.leftClick}>≪</div>}
+            {arrayRightEnd ? <div></div> : <div id="collapsed-right-arrow" onClick={this.rightClick}>≫</div>}
           </div>
         </div>
       )
@@ -104,16 +114,17 @@ class Photos extends React.Component {
       return (
         <div class="expanded-view">
           <div class="expanded-subPhoto">
-            {subPhotos.map((photo) =>
+            {subPhotosArray.map((photo) =>
               <div>
-                <img class="styles-item-1-2-1" id="subPhoto" src={photo.url} width="50" height="70" onClick={this.photoClick}></img>
+                <img class="styles-item-1-2-1" src={photo.url} width="50" height="70" onClick={this.photoClick}></img>
               </div>
             )}
           </div>
           <img class="expanded-mainPhoto" src={mainPhotoArray[mainPhotoIndex].url} width="575" height="700"></img>
           <div class="expanded-magnifying-glass" onClick={this.expandClick}>🔍</div>
-          <div id="expanded-left-arrow" onClick={this.leftClick}>←</div>
-          <div id="expanded-right-arrow" onClick={this.rightClick}>→</div>
+          {/* <img class="expanded-magnifying-glass" src={zoomIcon} onclick={this.expandClick}></img> */}
+          {arrayLeftEnd ? <div></div> : <div id="expanded-left-arrow" onClick={this.leftClick}>≪</div>}
+          {arrayRightEnd ? <div></div> : <div id="expanded-right-arrow" onClick={this.rightClick}>≫</div>}
         </div>
       )
     }
