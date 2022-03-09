fcd36203f084db3e7c2dbb791df72b70ab39bdd4
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-03-09 03:43:14 -0800

    styling Size and Comfort sliders complete

--- client/src/components/RatingsReviews/ComfortSlider.jsx
+++ client/src/components/RatingsReviews/ComfortSlider.jsx
@@ -1,5 +1,5 @@
-	import React from 'react';
-	import RatingsReviews from './RatingsReviews.jsx';
+import React from 'react';
+import RatingsReviews from './RatingsReviews.jsx';
 
 class ComfortSlider extends React.Component {
 	constructor(props) {
@@ -11,7 +11,7 @@ class ComfortSlider extends React.Component {
 	}
 
 	static getDerivedStateFromError(error) {
-		return {hasError: true};
+		return { hasError: true };
 	}
 
 	// componentDidMount() {
@@ -27,12 +27,12 @@ class ComfortSlider extends React.Component {
 	}
 
 	render() {
-		console.log('props in ComfortSlider are: ',  JSON.stringify(this.props.comfort));
+		console.log('props in ComfortSlider are: ', JSON.stringify(this.props.comfort));
 		// var comfort = this.props.comfort;
 		// if (this.state.hasError) {
 		// 	return <h1>Something went wrong.</h1>
 		// }
-		if (!this.state.comfort){
+		if (!this.state.comfort) {
 			return (
 				<div>
 					Loading.....
@@ -40,12 +40,25 @@ class ComfortSlider extends React.Component {
 			)
 		} else {
 			var comfort = this.props.comfort.Comfort.value;
+			var percentComfort = Math.round((comfort / 5) * 100);
 			return (
+				// <div className="comfort-slider" id="comfort-slider">
+				// 	<p>Comfort</p>
+				// 	<div>Poor     | {comfort}|     Perfect</div>
+				// </div>
 				<div className="comfort-slider" id="comfort-slider">
 					<p>Comfort</p>
-					{/* <div>comfort value: {comfort}</div> */}
-					{/* <div>{JSON.stringify(comfort)}</div> */}
-					<div>Poor     | {comfort}|     Perfect</div>
+					<div className="comfort-container">
+						<div className="comfort-side"></div>
+						<div className="comfort-middle"></div>
+						<div className="comfort-side"></div>
+						<div className="comfort-marker" style={{ marginLeft: `${percentComfort}%` }}>&#9660;</div>
+					</div>
+					<div className="comfort-explanation">
+						<div className="side">Too Small</div>
+						<div className="middle"></div>
+						<div className="side">Perfect</div>
+					</div>
 				</div>
 
 			);
--- client/src/components/RatingsReviews/SizeSlider.jsx
+++ client/src/components/RatingsReviews/SizeSlider.jsx
@@ -4,16 +4,44 @@ class SizeSlider extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			size: props.size
-		}
+			size: 0,
+			percentSize: 0
+		};
+		// this.setPercentSize = this.setPercentSize.bind(this);
 	}
+
+	// componentDidMount() {
+	// 	this.setState({
+	// 		size: this.props.size.Fit.value
+	// 	}, this.setPercentSize);
+	// }
+
+	// setPercentSize() {
+	// 	var percentSize = Math.round(this.props.size.Fit.value / 5) * 100;
+	// 	this.setState({
+	// 		percentSize: percentSize
+	// 	});
+	// }
 
 	render() {
 		var size = this.props.size.Fit.value;
+		console.log('size in SizeSlider is: ', size);
+		var percentSize = Math.round((size / 5) * 100);
+		console.log('percentSize in SizeSlider is: ', percentSize);
 		return (
 			<div className="size-slider" id="size-slider">
 				<p>Size</p>
-				<div>Too Small     |     {size}      |      Too Large</div>
+				<div className="size-container">
+					<div className="size-bar"></div>
+					<div className="size-bar"></div>
+					<div className="size-bar"></div>
+					<div className="size-marker" style={{ marginLeft: `${percentSize}%` }}>&#9660;</div>
+				</div>
+				<div className="size-explanation">
+					<div className="too-small">Too Small</div>
+					<div className="perfect">Perfect</div>
+					<div className="too-large">Too Large</div>
+				</div>
 			</div>
 
 		);
