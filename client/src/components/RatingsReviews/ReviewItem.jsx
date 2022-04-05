f9913174ec0e597f6928365119142b1dda978f6d
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-04-05 05:29:56 -0700

    Show More or Less Complete

--- client/src/components/RatingsReviews/ReviewItem.jsx
+++ client/src/components/RatingsReviews/ReviewItem.jsx
@@ -15,11 +15,28 @@ class ReviewItem extends React.Component {
 			helpfulness: 0,
 			markedHelpful: false,
 			reported: false,
-			photos: []
+			photos: [],
+			show_more: true,
+			short_review: '',
+			length: 0
 		}
 		this.getRecommendation = this.getRecommendation.bind(this);
 		this.markReviewHelpful = this.markReviewHelpful.bind(this);
 		this.reportReview = this.reportReview.bind(this);
+		this.showMore = this.showMore.bind(this);
+		this.showLess = this.showLess.bind(this);
+	}
+
+	showMore() {
+		this.setState({
+			show_more: true
+		});
+	}
+
+	showLess() {
+		this.setState({
+			show_more: false
+		});
 	}
 
 	getRecommendation(event) {
@@ -51,12 +68,32 @@ class ReviewItem extends React.Component {
 	}
 
 	componentDidMount() {
+		// console.log('this.props.review.body.split is: ', this.props.review.body.split(' '));
+		// reviewArr = this.props.review.body.split(' ');
+		var short_review = '';
+		if (this.props.review.body.length > 250) {
+			// for (var i = 0; i < 250; i++) {
+			// 	short_review.push(this.props.review.body[i]);
+			// }
+			short_review = this.props.review.body.slice(0, 250);
+			this.setState({
+				show_more: false
+			});
+		} else {
+			this.setState({
+				show_more: true
+			})
+		}
+		// var short_review = shortReviewArr.join(' ');
+
 		this.setState({
 			review: this.props.review,
 			ratingReview: this.props.review.rating,
 			helpfulness: this.props.review.helpfulness,
 			reviewID: this.props.review.review_id,
-			photos: this.props.review.photos
+			photos: this.props.review.photos,
+			short_review: short_review,
+			length: this.props.review.body.length
 		});
 	}
 
@@ -83,63 +120,90 @@ class ReviewItem extends React.Component {
 		if (day.length < 2) {
 			day = '0' + day;
 		}
-		console.log('date is: ', date);
-		return (
-			<div className="wrapper">
-				<div className="one" id="review-list">
-					<div className="star-review-container">
-						<StarRating num={this.props.review.rating} />
-						<div className="user-and-date">
-							{this.state.review.reviewer_name}, {month} {day}, {year}
-						</div>
+		// var reviewArr = this.state.review.body.split(' ');
+		var length = this.state.length;
+		let display;
+		console.log('length is: ', length);
+		if (this.state.length > 250) {
+
+			if (!this.state.show_more) {
+				// short reviews here with button, when button is pressed, switch show_more state to yes, then render based on yes or no
+				display = <div>
+					<div>{this.state.short_review}...
+						<div className="show-more-less" onClick={this.showMore}>&nbsp;&nbsp;&nbsp;Show more</div>
 					</div>
-					<h3 className="review-summary">{this.state.review.summary}</h3>
-					<p className="review-body">{this.state.review.body}</p>
-					<div className="helpful">Helpful?
-						{
-							!this.state.markedHelpful
-								?
-								<span className="helpful-unmarked" onClick={this.markReviewHelpful}>Yes</span>
-								:
-								<span className="helpful-marked">   Yes   </span>
-						}
-						{
-							`(${this.state.helpfulness})   |   `
-						}
-						{
-							!this.state.reported
-								?
-								<span className="report-unmarked" onClick={this.reportReview}>   Report   </span>
-								:
-								<span className="report-marked">Reported</span>
-						}
-					</div>
-					{this.state.review.recommend &&
-						<div>
-							✔ I recommend this product
-						</div>
-					}
-					<div className="productPhoto">
-						{this.state.photos.length > 0
-							? <div className="review-photo-container">
-								<sub>Review Photos: </sub>
-								<br></br><br></br>
-								{
-									this.state.review.photos.map((photo) =>
-										<img className="review-photo" src={photo.url} key={photo.id}></img>
-									)
-								}
-							</div>
-							:
-							<div></div>
-							//  <div>
-							// 	<br />
-							// 	<button className="submit-btn">Submit Photos</button>
-							// </div>
-						}
-					</div>
-					<hr></hr>
 				</div>
+			} else if (this.state.show_more) {
+				// full review here
+				display = <div>
+					<div className="review-body">{this.state.review.body}
+						<div className="show-more-less" onClick={this.showLess}>&nbsp;&nbsp;&nbsp;Show less</div>
+					</div>
+				</div>
+			}
+		} else {
+			display = <div className="review-body">{this.state.review.body}</div>
+
+		}
+
+
+	return (
+			<div className="wrapper" >
+			<div className="one" id="review-list">
+				<div className="star-review-container">
+					<StarRating num={this.props.review.rating} />
+					<div className="user-and-date">
+						{this.state.review.reviewer_name}, {month} {day}, {year}
+					</div>
+				</div>
+				<h3 className="review-summary">{this.state.review.summary}</h3>
+				<div>{display}</div>
+				<br></br>
+				<div className="helpful">Helpful?
+					{
+						!this.state.markedHelpful
+							?
+							<span className="helpful-unmarked" onClick={this.markReviewHelpful}>Yes</span>
+							:
+							<span className="helpful-marked">   Yes   </span>
+					}
+					{
+						`(${this.state.helpfulness})   |   `
+					}
+					{
+						!this.state.reported
+							?
+							<span className="report-unmarked" onClick={this.reportReview}>   Report   </span>
+							:
+							<span className="report-marked">Reported</span>
+					}
+				</div>
+				{this.state.review.recommend &&
+					<div>
+						✔ I recommend this product
+					</div>
+				}
+				<div className="productPhoto">
+					{this.state.photos.length > 0
+						? <div className="review-photo-container">
+							<sub>Review Photos: </sub>
+							<br></br><br></br>
+							{
+								this.state.review.photos.map((photo) =>
+									<img className="review-photo" src={photo.url} key={photo.id}></img>
+								)
+							}
+						</div>
+						:
+						<div></div>
+						//  <div>
+						// 	<br />
+						// 	<button className="submit-btn">Submit Photos</button>
+						// </div>
+					}
+				</div>
+				<hr></hr>
+			</div>
 			</div>
 		);
 	}
