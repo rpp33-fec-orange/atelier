37ba3e8dc53d16e80ff8e9f68bf92d971064cbda
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-31 04:39:27 -0700

    return webpack.config.js to normal and fixed sideEffects in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            // presets: ['@babel/preset-env', '@babel/preset-react']
-            presets: [
-              [ 'es2015', { modules: false }]
-            ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
+            // presets: [
+            //   [ 'es2015', { modules: false }]
+            // ]
+            //     ^^^^^^ deprecated
           }
         }
       }
